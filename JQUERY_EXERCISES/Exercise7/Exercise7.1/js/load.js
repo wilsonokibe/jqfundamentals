632aacd22e0e73678c7dd3deaa9793d021e204eb
Wilson Okibe <wilson@dealdey.com>
2015-08-27 18:26:47 +0100

    Push Track 7

---
+++ JQUERY_EXERCISES/Exercise7/Exercise7.1/js/load.js
@@ -0,0 +1,52 @@
+"use strict";
+/*
+1.Create a target div after the headline for each blog post and store a reference to it on the headline element using $.fn.data.
+
+2.Bind a click event to the headline that will use the $.fn.load method to load the appropriate content from /exercises/data/blog.html 
+  into the target div. Don't forget to prevent the default action of the click event.
+
+Note that each blog headline in index.html includes a link to the post. You'll need to leverage the href of that link to get the proper content from blog.html. Once you have the href, here's one way to process it into an ID that you can use as a selector in $.fn.load:
+
+var href = 'blog.html#post1';
+var tempArray = href.split('#');
+var id = '#' + tempArray[1];
+Remember to make liberal use of console.log to make sure you're on the right path!
+*/
+
+class Blog {
+
+  newDivForEachBlog() {
+    var $targetDiv = $('<div></div>');
+    $('div.module ul > li h3').each(function(index, element) {
+      var $targetDivClone = $targetDiv.clone();
+      $targetDivClone.attr('id', 'post'+(index+1));
+      $targetDivClone.insertAfter($(this));
+      $(this).data('id', 'post'+(index+1));
+    });
+    this.bindEvent();
+  }
+
+  bindEvent() {
+    let self = this;
+    $('div.module ul > li a').click(function(event) {
+      event.preventDefault();
+      self.getBlogContent($(this));
+      if($('div.module ul > li a').not($(this))) {
+        $('div.module ul > li a').parent().siblings('div').hide();
+        $(this).parent().siblings('div').show();
+      }
+    });
+  }
+
+  getBlogContent(selector) {
+    var href = $(selector).attr('href');
+    var tempArray = href.split('#');
+    var id = '#' + tempArray[1];
+    $('div.module ul > li div').load('./data/blog.html '+id);
+  }
+}
+
+$(document).ready(function() {
+  const blog = new Blog();
+  blog.newDivForEachBlog();
+});
---
+++ JQUERY_EXERCISES/Exercise7/Exercise7.2/js/specials.js
@@ -0,0 +1,87 @@
+"use strict";
+/*
+Open the file /exercises/index.html in your browser. Use the file /exercises/js/specials.js. 
+Your task is to show the user details about the special for a given day when the user selects 
+a day from the select dropdown.
+
+1. Append a target div after the form that's inside the #specials element; this will be where 
+you put information about the special once you receive it.
+
+2. Bind to the change event of the select element; when the user changes the selection, send an 
+Ajax request to /exercises/data/specials.json.
+When the request returns a response, use the value the user selected in the select (hint: $.fn.val) 
+to look up information about the special in the JSON response.
+Add some HTML about the special to the target div you created.
+
+Finally, because the form is now Ajax-enabled, remove the submit button from the form.
+
+Note that we're loading the JSON every time the user changes their selection. How could we change 
+the code so we only make the request once, and then use a cached response when the user changes 
+their choice in the select?
+*/
+
+class User {
+  constructor() {
+    this.cachedData = null;
+  }
+
+  newDivElement() {
+    $('#specials form').after($('<div>DIV ELEMENT</div>'));
+    this.userSelection();
+  }
+
+  userSelection() {
+    let self = this;
+    var $option = $('#specials select');
+    $option.on('change', function() {
+      var attribute = $(this).val(); 
+      self.hideOrShow(attribute);   
+    });
+  }
+
+  hideOrShow(attribute) {
+    if(attribute == "") { 
+      $('#specials div').hide();
+    }
+    else { 
+      $('#specials div').show();
+      if(this.cachedData != null) {
+        this.displayRequestResult(attribute, this.cachedData);
+      } else {
+        this.requestAndGetJSON(attribute);
+      }   
+    }
+  }
+
+  requestAndGetJSON(attribute) {
+    let self = this;
+    $.getJSON('./data/specials.json', function(data) {
+      self.cachedData = data;      
+      self.displayRequestResult(attribute, data);
+    });
+  }
+
+  displayRequestResult(attribute, data) {
+    var html = '';
+    html += '<h3>'+data[attribute].title+'</h3>';
+    html += '<p style="color:'+data[attribute].color+'"><img src="'+data[attribute].image+'"/></br>'+data[attribute].text+'</p>';
+    $('#specials div').html(html);
+  }
+
+  removeSubmitButton() {
+    $('li.buttons').remove();
+  }
+}
+
+$(document).ready(function() {
+  const user = new User();
+  user.removeSubmitButton();
+  user.newDivElement();
+})
+
+
+/*
+<li class="buttons">
+  <input type="submit" class="input_submit" value="Go" />
+</li>
+*/
